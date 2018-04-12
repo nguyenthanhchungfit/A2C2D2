bb73637d2c30853b38711d0fec4ee71893bb8769
Thanh Chung <nguyenthanhchungfit@gmail.com>
2018-04-13 01:02:09 +0700

    Initial commit

---
+++ app/src/main/java/com/nguyenthanhchung/carop2p/MainGameActivity.java
@@ -0,0 +1,13 @@
+package com.nguyenthanhchung.carop2p;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_game);
+    }
+}
