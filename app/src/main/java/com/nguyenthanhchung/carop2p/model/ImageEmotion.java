42c65911c48cd5ad232249593713ee7b74c01efc
Thanh Chung <nguyenthanhchungfit@gmail.com>
2018-04-18 21:13:32 +0700

    Chuyển boardgame sang fragment, tích hợp vào MainGameActivity

---
+++ app/src/main/java/com/nguyenthanhchung/carop2p/model/ImageEmotion.java
@@ -0,0 +1,21 @@
+package com.nguyenthanhchung.carop2p.model;
+
+/**
+ * Created by Nguyen Thanh Chung on 2018-04-13.
+ */
+
+public class ImageEmotion {
+    private int idImage;
+
+    public ImageEmotion(int idImage) {
+        this.idImage = idImage;
+    }
+
+    public int getIdImage() {
+        return idImage;
+    }
+
+    public void setIdImage(int idImage) {
+        this.idImage = idImage;
+    }
+}
