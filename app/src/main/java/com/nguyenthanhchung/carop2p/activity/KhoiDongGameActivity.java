2aba4bcf2330f7c833b68e204a825f33cb23a0e5
nttuananh <nttuananh@gmail.com>
2018-04-28 07:25:16 +0700

    add xu ly 3 button: Guide, info, exit

--- app/src/main/java/com/nguyenthanhchung/carop2p/activity/KhoiDongGameActivity.java
+++ app/src/main/java/com/nguyenthanhchung/carop2p/activity/KhoiDongGameActivity.java
@@ -1,5 +1,8 @@
 package com.nguyenthanhchung.carop2p.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
@@ -7,18 +10,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.text.method.ScrollingMovementMethod;
 import com.nguyenthanhchung.carop2p.R;
+import com.nguyenthanhchung.carop2p.adapter.SinhVienAdapter;
+import com.nguyenthanhchung.carop2p.fragment.HuongDanFragment;
+import com.nguyenthanhchung.carop2p.fragment.ThucHienFragment;
+import com.nguyenthanhchung.carop2p.model.SinhVien;
+
+import java.util.ArrayList;
 
 public class KhoiDongGameActivity extends AppCompatActivity {
     MediaPlayer background_song;
     MediaPlayer button_click_sound;
+    FrameLayout frameLayout;
+    FragmentTransaction fragmentTransaction;
+    HuongDanFragment huongDanFragment;
+    ThucHienFragment thucHienFragment;
     Button btnPlay;
+    Button btnGuide;
+    Button btnInfo;
+    Button btnExit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_khoi_dong_game);
+        frameLayout = findViewById(R.id.frameContent);
+        frameLayout.setVisibility(View.INVISIBLE);
+
         background_song = MediaPlayer.create(KhoiDongGameActivity.this,R.raw.background_sound);
         background_song.setLooping(true);
         button_click_sound = MediaPlayer.create(KhoiDongGameActivity.this,R.raw.button_click);
@@ -26,6 +49,9 @@ public class KhoiDongGameActivity extends AppCompatActivity {
 
         //Init button
         btnPlay = findViewById(R.id.btnPlay);
+        btnGuide = findViewById(R.id.btnGuide);
+        btnInfo = findViewById(R.id.btnInfo);
+        btnExit = findViewById(R.id.btnExit);
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,16 +89,60 @@ public class KhoiDongGameActivity extends AppCompatActivity {
 
     public void guideGame(View view) {
         button_click_sound.start();
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        huongDanFragment = new HuongDanFragment();
+        fragmentTransaction.add(R.id.frameContent, huongDanFragment, "guide");
+        fragmentTransaction.commit();
+        hideBtn();
         //code switch to guide game screen here
+    }
+
+    public void exitGuide(View view) {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        huongDanFragment = (HuongDanFragment) getFragmentManager().findFragmentByTag("guide");
+        fragmentTransaction.remove(huongDanFragment);
+        fragmentTransaction.commit();
+        showBtn();
+    }
+
+    public void exitInfo(View view) {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        thucHienFragment = (ThucHienFragment) getFragmentManager().findFragmentByTag("info");
+        fragmentTransaction.remove(thucHienFragment);
+        fragmentTransaction.commit();
+        showBtn();
     }
 
     public void showInfo(View view) {
         button_click_sound.start();
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        thucHienFragment = new ThucHienFragment();
+        fragmentTransaction.add(R.id.frameContent, thucHienFragment, "info");
+        fragmentTransaction.commit();
+        hideBtn();
         //code switch to show info screen here
     }
 
     public void exitGame(View view) {
         button_click_sound.start();
+        finish();
         //code switch to exit screen here
     }
+
+    public void hideBtn() {
+        btnPlay.setVisibility(View.INVISIBLE);
+        btnGuide.setVisibility(View.INVISIBLE);
+        btnInfo.setVisibility(View.INVISIBLE);
+        btnExit.setVisibility(View.INVISIBLE);
+        frameLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void showBtn() {
+        frameLayout.setVisibility(View.INVISIBLE);
+        btnPlay.setVisibility(View.VISIBLE);
+        btnGuide.setVisibility(View.VISIBLE);
+        btnInfo.setVisibility(View.VISIBLE);
+        btnExit.setVisibility(View.VISIBLE);
+    }
+
 }
